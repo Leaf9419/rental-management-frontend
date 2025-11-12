ce3a31b1de91908aca27504b9e1305638f4622cf
John <wtyyiiuroc86@gmail.com>
2025-11-12 16:07:41 +0800

    feat: 路徑模組化寫法

---
+++ src/backend/admin/routes.js
@@ -0,0 +1,7 @@
+export default [
+    // {
+    //     path: "/property-test1",
+    //     name: "PropertyTest1",
+    //     component: () => import("@/frontend/property/views/Property.vue"),
+    // },
+];
